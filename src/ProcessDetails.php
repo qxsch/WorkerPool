56d9ddcf9a0f0dd1578481b11858161a450ab1b8
Marco Falkenberg <mf@unidatagroup.de>
2014-05-22 15:20:05 +0200

    Manage processes in dedicated collection.

--- src/ProcessDetails.php
+++ src/ProcessDetails.php
@@ -14,16 +14,6 @@ class ProcessDetails {
 	protected $socket;
 
 	/**
-	 * @var bool
-	 */
-	protected $isFree;
-
-	/**
-	 * @var array
-	 */
-	protected static $freeProcessIds = array();
-
-	/**
 	 * @param int $pid
 	 * @param SimpleSocket $socket
 	 */
@@ -31,14 +21,6 @@ class ProcessDetails {
 		$this->pid = $pid;
 		$this->socket = $socket;
 		$this->socket->annotation['pid'] = $pid;
-		$this->setIsFree(TRUE);
-	}
-
-	/**
-	 * @return boolean
-	 */
-	public function isFree() {
-		return $this->isFree;
 	}
 
 	/**
@@ -53,31 +35,5 @@ class ProcessDetails {
 	 */
 	public function getSocket() {
 		return $this->socket;
-	}
-
-	/**
-	 * @param boolean $isFree
-	 */
-	public function setIsFree($isFree) {
-		$this->isFree = $isFree;
-		if ($isFree) {
-			self::$freeProcessIds[$this->pid] = TRUE;
-		} else {
-			unset(self::$freeProcessIds[$this->pid]);
-		}
-	}
-
-	/**
-	 * @return int|NULL
-	 */
-	public static function getFreeProcessId() {
-		$freeProcessIds = array_keys(self::$freeProcessIds);
-		return count($freeProcessIds) > 0 ? $freeProcessIds[0] : NULL;
-	}
-
-	public function killProcess() {
-		$this->setIsFree(FALSE);
-		@socket_close($this->socket->getSocket());
-		@posix_kill($this->pid, 9);
 	}
 }
