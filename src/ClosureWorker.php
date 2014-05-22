1415bd1d9cc98d9087b3d913830a8c9ebc1cece6
Marco Falkenberg <mf@unidatagroup.de>
2014-05-22 16:18:48 +0200

    Do not call empty closures.

--- src/ClosureWorker.php
+++ src/ClosureWorker.php
@@ -33,14 +33,6 @@ class ClosureWorker implements Worker {
 	 */
 	public function __construct(\Closure $run, \Closure $create = NULL, \Closure $destroy = NULL) {
 		$this->storage = new \ArrayObject();
-		if (is_null($create)) {
-			$create = function ($semaphore, $storage) {
-			};
-		}
-		if (is_null($destroy)) {
-			$destroy = function ($semaphore, $storage) {
-			};
-		}
 		$this->create = $create;
 		$this->run = $run;
 		$this->destroy = $destroy;
@@ -54,7 +46,9 @@ class ClosureWorker implements Worker {
 	 */
 	public function onProcessCreate(Semaphore $semaphore) {
 		$this->semaphore = $semaphore;
-		$this->create->__invoke($this->semaphore, $this->storage);
+		if($this->create !== NULL){
+			$this->create->__invoke($this->semaphore, $this->storage);
+		}
 	}
 
 	/**
@@ -63,7 +57,9 @@ class ClosureWorker implements Worker {
 	 * @throws \Exception in case of a processing Error an Exception will be thrown
 	 */
 	public function onProcessDestroy() {
-		$this->destroy->__invoke($this->semaphore, $this->storage);
+		if($this->destroy !== NULL){
+			$this->destroy->__invoke($this->semaphore, $this->storage);
+		}
 	}
 
 	/**
