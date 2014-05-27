3c282e67648795ed8d9c97a5767b39eeb59bafb6
Marco Falkenberg <mf@unidatagroup.de>
2014-05-27 14:32:39 +0200

    Add autloader for tests and examples.

---
+++ autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+spl_autoload_register(function ($className) {
+	$pathMap = array(
+		'QXS\WorkerPool' => 'src',
+		'QXS\Tests\WorkerPool' => 'tests'
+	);
+	foreach ($pathMap as $namespace => $directory) {
+		$strLen = strlen($namespace);
+		if ($strLen >= $className && substr($className, 0, $strLen) === $namespace) {
+			$path = __DIR__ . DIRECTORY_SEPARATOR . $directory . substr($className, $strLen) . '.php';
+			$path = str_replace('\\', DIRECTORY_SEPARATOR, $path);
+			require_once($path);
+			return TRUE;
+		}
+	}
+	return FALSE;
+});
--- examples/simpleExample.php
+++ examples/simpleExample.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(__DIR__.'/../vendor/autoload.php');
+require_once(__DIR__ . '/../autoload.php');
 
 
 $wp=new \QXS\WorkerPool\WorkerPool();
