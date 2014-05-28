a059525f4bfb5b45c5673cbf1ed43ff60103217b
Marco Falkenberg <mf@unidatagroup.de>
2014-05-28 09:02:00 +0200

    Bugfix: Strange behaviour array_shift
    Sometimes array_shift destroys the keys of the array.

--- src/ProcessDetailsCollection.php
+++ src/ProcessDetailsCollection.php
@@ -122,7 +122,9 @@ class ProcessDetailsCollection implements \IteratorAggregate {
 		if ($this->getFreeProcessesCount() === 0) {
 			return NULL;
 		}
-		$freePid = array_shift($this->freeProcessIds);
+
+		$freePid = array_pop($this->freeProcessIds);
+
 		if ($freePid === NULL) {
 			return NULL;
 		}
