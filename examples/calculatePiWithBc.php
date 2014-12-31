8511f1a9b87725992dbcbc023f787d7aa42282dc
qxsch <qxsch@users.noreply.github.com>
2014-12-31 01:11:15 +0100

    added pi calc using BCMATH

---
+++ examples/calculatePiWithBc.php
@@ -0,0 +1,92 @@
+<?php
+require_once(__DIR__ . '/../autoload.php');
+
+
+use QXS\WorkerPool\WorkerPool;
+use QXS\WorkerPool\WorkerInterface;
+use QXS\WorkerPool\Semaphore;
+
+
+/**
+ * The PI Worker
+ */
+class PiWorker implements WorkerInterface {
+	/** @var int the number of steps */
+	protected $num_steps;
+	/** @var float the step */
+	protected $step;
+
+	/**
+	 * The constructor
+	 */
+	public function __construct($num_steps, $step) {
+		$this->num_steps = (int) $num_steps;
+		$this->step = (string) $step;
+
+	}
+
+	/**
+	 * After the worker has been forked into another process
+	 *
+	 * @param \QXS\WorkerPool\Semaphore $semaphore the semaphore to run synchronized tasks
+	 * @throws \Exception in case of a processing Error an Exception will be thrown
+	 */
+	public function onProcessCreate(Semaphore $semaphore) { }
+
+	/**
+	 * Before the worker process is getting destroyed
+	 *
+	 * @throws \Exception in case of a processing Error an Exception will be thrown
+	 */
+	public function onProcessDestroy() { }
+
+	/**
+	 * run the work
+	 *
+	 * @param \Serializable $input the data, that the worker should process
+	 * @return \Serializable Returns the result
+	 * @throws \Exception in case of a processing Error an Exception will be thrown
+	 */
+	public function run($input) {
+		list($part_number, $part_step) = $input;
+		$sum='0';
+		for ($i = $part_number; $i < $this->num_steps; $i += $part_step) {
+			//$x = ($i + 0.5) * $this->step;
+			//$sum += 4.0 / (1.0 + $x * $x);
+			$x = bcmul(bcadd($i, '0.5'), $this->step);
+			$sum = bcadd($sum, bcdiv('4.0', bcadd('1.0', bcmul($x, $x))));
+		}
+		return $sum;
+	}
+
+}
+
+
+
+bcscale(100); // sets the scale
+$num_steps = 1000000; // setps - sets the calculation detail
+$part_step = 8; // parallelization degree set to number of cores
+
+$step = bcdiv('1.0', (string)$num_steps);
+
+$wp=new WorkerPool();
+$wp->setWorkerPoolSize($part_step)
+   ->create(new PiWorker($num_steps, $step));
+
+for($i = 0; $i < $part_step; $i++) {
+	$wp->run(array($i, $part_step));
+}
+
+
+$wp->waitForAllWorkers(); // wait for all workers
+
+$sum=0;
+foreach($wp as $result) {
+	$sum=bcadd($sum, $result['data']);
+}
+
+$pi = bcmul($step, $sum);
+
+echo "PI is $pi\n";
+
+
