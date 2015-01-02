a7ce0b1dd3de89385d71421bb14df0dbf69cf3ec
qxsch <qxsch@users.noreply.github.com>
2015-01-02 01:37:17 +0100

    improved the documentation

--- src/SerializableWorkerClosure.php
+++ src/SerializableWorkerClosure.php
@@ -18,23 +18,34 @@ class SerializableWorkerClosure {
 
 	/**
 	 * The constructor
-	 * @param \Closure $run  the closure, that should be run
-	 * @param mixed $input any data, that should be processed
+	 * @param \Closure $run  the closure, that the worker should run
+	 * @param \Serializable $input the data, that the worker should process
 	 */
 	public function __construct(\Closure $run, $input = NULL) {
 		$this->run = new SerializableClosure($run);
 		$this->input = $input;
 	}
 
-
+	/**
+	 * Sets the data, that should be processed
+	 * @param \Serializable $input the data, that the worker should process
+	 */
 	public function setInput($input) {
 		$this->input = $input;
 		return $this;
 	}
+
+	/**
+	 * Gets the data, that should be processed
+	 * @return \Serializable the data, that the worker should process
+	 */
 	public function getInput() {
 		return $this->input;
 	}
 
+	/**
+	 * @return SerializableClosure the closure, that the worker should run
+	 */
 	public function getSerializableClosure() {
 		return $this->run;
 	}
