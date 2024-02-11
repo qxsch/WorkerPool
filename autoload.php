5700bd027f0e7da4de6072c68bb6eda6584ecea0
qxsch <qxsch@users.noreply.github.com>
2024-02-11 11:22:08 +0100

    fixed autoload for php8

--- autoload.php
+++ autoload.php
@@ -7,7 +7,7 @@ spl_autoload_register(function ($className) {
 	);
 	foreach ($pathMap as $namespace => $directory) {
 		$strLen = strlen($namespace);
-		if ($strLen >= $className && substr($className, 0, $strLen) === $namespace) {
+		if ($strLen <= strlen($className) && substr($className, 0, $strLen) === $namespace) {
 			$path = __DIR__ . DIRECTORY_SEPARATOR . $directory . substr($className, $strLen) . '.php';
 			$path = str_replace('\\', DIRECTORY_SEPARATOR, $path);
 			require_once($path);
